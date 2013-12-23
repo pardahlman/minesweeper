7de9653714dac248f3f12d7939a341551cfab307
par.dahlman <par.dahlman@valtech.se>
2013-12-23 13:28:18 +0100

    Adding Jasime runners and initial tests

---
+++ MineSweaper/Jasmine/Specifications/BrickFlipperSpecifications.js
@@ -0,0 +1,52 @@
+/// <reference path="../../Script/BrickFlipper.ts"/>
+/// <reference path="../../Script/Brick.ts"/>
+describe("The BrickFlipper", function () {
+    it("Should be defined", function () {
+        expect(BrickFlipper).toBeDefined();
+    });
+
+    describe("The 'flipBrick' method", function () {
+        var brick, neighbour, flipper;
+
+        beforeEach(function () {
+            flipper = new BrickFlipper();
+            brick = new Brick();
+            neighbour = new Brick();
+
+            brick.adjacentBricks.push(neighbour);
+        });
+
+        it("Should set state to 'facaingUp' on brick being flipped", function () {
+            /* Test */
+            flipper.flip(brick);
+
+            /* Assert */
+            expect(brick.state).toEqual(2 /* FacingUp */);
+        });
+
+        it("Should not flip neighbour if brick is of type 'Bomb'", function () {
+            /* Setup */
+            brick.type = 2 /* Bomb */;
+            spyOn(flipper, 'flip').andCallThrough();
+
+            /* Test */
+            flipper.flip(brick);
+
+            /* Assert */
+            expect(flipper.flip).not.toHaveBeenCalledWith(neighbour);
+        });
+
+        it("Should call 'flip' in neighbours if type is 'Normal'", function () {
+            /* Setup */
+            brick.type = 1 /* Normal */;
+            spyOn(flipper, 'flip').andCallThrough();
+
+            /* Test */
+            flipper.flip(brick);
+
+            /* Assert */
+            expect(flipper.flip).toHaveBeenCalledWith(neighbour);
+        });
+    });
+});
+//# sourceMappingURL=BrickFlipperSpecifications.js.map
---
+++ MineSweaper/Jasmine/Specifications/BrickFlipperSpecifications.ts
@@ -0,0 +1,56 @@
+/// <reference path="../../Script/BrickFlipper.ts"/>
+/// <reference path="../../Script/Brick.ts"/>
+
+describe("The BrickFlipper", ()=> {
+    it("Should be defined", ()=> {
+        expect(BrickFlipper).toBeDefined();
+    });
+
+    describe("The 'flipBrick' method", () => {
+        var brick: Brick,
+            neighbour: Brick,
+            flipper: BrickFlipper;
+
+        beforeEach(()=> {
+            flipper = new BrickFlipper();
+            brick = new Brick();
+            neighbour = new Brick();
+
+            brick.adjacentBricks.push(neighbour);
+        });
+
+        it("Should set state to 'facaingUp' on brick being flipped", ()=> {
+            /* Test */
+            flipper.flip(brick);
+
+            /* Assert */
+            expect(brick.state).toEqual(BrickState.FacingUp);
+        });
+
+        it("Should not flip neighbour if brick is of type 'Bomb'", ()=> {
+            /* Setup */
+            brick.type = BrickType.Bomb;
+            spyOn(flipper, 'flip').andCallThrough();
+
+            /* Test */
+            flipper.flip(brick);
+
+            /* Assert */
+            expect(flipper.flip).not.toHaveBeenCalledWith(neighbour);
+        });
+
+        it("Should call 'flip' in neighbours if type is 'Normal'", () => {
+            /* Setup */
+            brick.type = BrickType.Normal;
+            spyOn(flipper, 'flip').andCallThrough();
+
+            /* Test */
+            flipper.flip(brick);
+
+            /* Assert */
+            expect(flipper.flip).toHaveBeenCalledWith(neighbour);
+
+        });
+    });
+}) 
+
