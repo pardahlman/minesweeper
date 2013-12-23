4b8ebe4661fc5e693f7293ee6be4bf69ee27ff72
par.dahlman <par.dahlman@valtech.se>
2013-12-23 19:13:28 +0100

    CLEAN: Minor formating on BrickFlipper.ts

--- MineSweaper/Script/BrickFlipper.ts
+++ MineSweaper/Script/BrickFlipper.ts
@@ -3,7 +3,7 @@ class BrickFlipper implements IFlipper {
     flip(brick: Brick): void {
         brick.state = BrickState.FacingUp;
         if (brick.type == BrickType.Normal) {
-            brick.adjacentBricks.forEach(neighbour=>  this.flip(neighbour) );
+            brick.adjacentBricks.forEach(neighbour => this.flip(neighbour) );
         }
 
     }
