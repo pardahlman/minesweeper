caabe1a61b5b156f0071dc13cc8a53b5f8f10bbe
par.dahlman <par.dahlman@valtech.se>
2013-12-24 21:09:22 +0100

    Introducing the BrickGenerator
    Over-engineered indeed... but this one creates the bricks

---
+++ MineSweaper/Script/BrickGenerator.ts
@@ -0,0 +1,16 @@
+class BrickGenerator {
+    public static createBricks(size: BoardSize): Array<Array<Brick>> {
+
+        var bricks = new Array<Array<Brick>>();
+
+        for (var columnIndex = 0; columnIndex < size.boardWidth; columnIndex++) {
+            var currentColumn = new Array<Brick>();
+            bricks.push(currentColumn);
+            for (var rowIndex = 0; rowIndex < size.boardHeight; rowIndex++) {
+                currentColumn.push(new Brick(rowIndex, columnIndex));
+            }
+        }
+
+        return bricks;
+    }
+} 
