91ac2d9ed0d3ebe6eaf62521bf1350f1e2de943c
par.dahlman <par.dahlman@valtech.se>
2013-12-27 12:17:03 +0100

    Adding ArrayExtensions with 'shuffle' method
    Just to try out the open-endedness of the TS interfaces, I implemented a
    suffle method that scrumles objects in the array.

---
+++ MineSweaper/Script/ArrayExtensions.ts
@@ -0,0 +1,25 @@
+interface Array {
+    shuffle() : void
+}
+
+Array.prototype.shuffle = function () {
+    
+    var currentIndex = this.length
+        , temporaryValue
+        , randomIndex
+    ;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = this[currentIndex];
+        this[currentIndex] = this[randomIndex];
+        this[randomIndex] = temporaryValue;
+    }
+
+};
