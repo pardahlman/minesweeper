3fc534a9a0f8f93c9a1ec5cc300a04947a05a179
par.dahlman <par.dahlman@valtech.se>
2014-01-02 10:32:56 +0100

    CLEAN: Removed generated js-files

--- MineSweaper/Script/Presentation/BrickViewModel.js
+++
@@ -1,9 +0,0 @@
-var BrickViewModel = (function () {
-    function BrickViewModel(brick) {
-        this.DisplayText = ko.computed(function () {
-            return "?";
-        });
-    }
-    return BrickViewModel;
-})();
-//# sourceMappingURL=BrickViewModel.js.map
