c07fd927211a80614bf8424f049ce7c568a0c809
par.dahlman <par.dahlman@valtech.se>
2014-01-02 18:31:07 +0100

    Writing some more tests to make sure relations are set property
    But this is sooo boring :) Pausing that for a while to get some other things
    done

--- MineSweaper/Jasmine/Specifications/GameSetupHelperSpecifications.ts
+++ MineSweaper/Jasmine/Specifications/GameSetupHelperSpecifications.ts
@@ -58,22 +58,48 @@ describe("The 'GameSetupHelper'", ()=> {
             GameSetupHelper.setBrickRelations(bricks);
         });
 
-        it("Should set correct neighbours for left top corner brick", ()=> {
+        it("Should set correct neighbours for bottom-left corner brick", ()=> {
             /* Assert */
+            var bottomLeft = bricks[0][0];
+
+            var leftNeighbour = bricks[1][0];
+            var verticalNeighbour = bricks[1][1];
+            var aboveNeighbour = bricks[0][1];
 
             // hack to get the toContain to work -- we get a circular reference otherwise
-            bricks[0][1].adjacentBricks = new Array<Brick>();
-            bricks[1][1].adjacentBricks = new Array<Brick>();
-            bricks[0][1].adjacentBricks = new Array<Brick>();
+            leftNeighbour.adjacentBricks = new Array<Brick>();
+            verticalNeighbour.adjacentBricks = new Array<Brick>();
+            aboveNeighbour.adjacentBricks = new Array<Brick>();
 
-            expect(bricks[0][0].adjacentBricks.length).toEqual(3);
-            expect(bricks[0][0].adjacentBricks).toContain(bricks[0][1]);
-            expect(bricks[0][0].adjacentBricks).toContain(bricks[1][1]);
-            expect(bricks[0][0].adjacentBricks).toContain(bricks[0][1]);
+            expect(bottomLeft.adjacentBricks.length).toEqual(3);
+            expect(bottomLeft.adjacentBricks).toContain(leftNeighbour);
+            expect(bottomLeft.adjacentBricks).toContain(verticalNeighbour);
+            expect(bottomLeft.adjacentBricks).toContain(aboveNeighbour);
         });
 
-        it("Should set correct neighbours for top right corner brick", () => {
+        it("Should set correct neighbours for top-left corner brick", () => {
             /* Assert */
+            var topLeft = bricks[0][size.boardHeight - 1];
+
+            var rightNeighbour = bricks[1][size.boardHeight - 1];
+            var verticalNeighbour = bricks[1][size.boardHeight - 2];
+            var belowNeighbour = bricks[0][size.boardHeight - 2];
+
+            // hack to get the toContain to work -- we get a circular reference otherwise
+            rightNeighbour.adjacentBricks = new Array<Brick>();
+            verticalNeighbour.adjacentBricks = new Array<Brick>();
+            belowNeighbour.adjacentBricks = new Array<Brick>();
+
+            expect(topLeft.adjacentBricks.length).toEqual(3);
+            expect(topLeft.adjacentBricks).toContain(rightNeighbour);
+            expect(topLeft.adjacentBricks).toContain(verticalNeighbour);
+            expect(topLeft.adjacentBricks).toContain(belowNeighbour);
+        });
+
+        it("Should set correct neighbours for top-right corner brick", () => {
+            /* Assert */
+            var topRight = bricks[size.boardWidth - 1][size.boardHeight - 1];
+
             var leftNeighbour = bricks[size.boardWidth - 2][size.boardHeight - 1];
             var verticalNeighbour = bricks[size.boardWidth - 2][size.boardHeight - 2];
             var belowNeighbour = bricks[size.boardWidth - 1][size.boardHeight - 2];
@@ -83,13 +109,57 @@ describe("The 'GameSetupHelper'", ()=> {
             verticalNeighbour.adjacentBricks = new Array<Brick>();
             belowNeighbour.adjacentBricks = new Array<Brick>();
 
-            var topRight = bricks[size.boardWidth - 1][size.boardHeight - 1];
-
             expect(topRight.adjacentBricks.length).toEqual(3);
             expect(topRight.adjacentBricks).toContain(leftNeighbour);
             expect(topRight.adjacentBricks).toContain(verticalNeighbour);
             expect(topRight.adjacentBricks).toContain(belowNeighbour);
         });
+
+        it("Should set correct neighbours for bottom-right corner brick", () => {
+            /* Assert */
+            var bottomRight = bricks[size.boardWidth - 1][0];
+
+            var leftNeighbour = bricks[size.boardWidth - 2][0];
+            var verticalNeighbour = bricks[size.boardWidth - 2][1];
+            var aboveNeighbour = bricks[size.boardWidth - 1][1];
+
+            // hack to get the toContain to work -- we get a circular reference otherwise
+            leftNeighbour.adjacentBricks = new Array<Brick>();
+            verticalNeighbour.adjacentBricks = new Array<Brick>();
+            aboveNeighbour.adjacentBricks = new Array<Brick>();
+
+            expect(bottomRight.adjacentBricks.length).toEqual(3);
+            expect(bottomRight.adjacentBricks).toContain(leftNeighbour);
+            expect(bottomRight.adjacentBricks).toContain(verticalNeighbour);
+            expect(bottomRight.adjacentBricks).toContain(aboveNeighbour);
+        });
+
+        it("Should set correct neighbours for top-edge brick", () => {
+            /* Assert */
+            var topEdge = bricks[1][size.boardHeight -1];
+
+            var leftNeighbour = bricks[0][size.boardHeight - 1];
+            var rightBrick = bricks[2][size.boardHeight - 1];
+            var belowRight = bricks[2][size.boardHeight - 2];
+            var belowNeighbour = bricks[1][size.boardHeight - 2];
+            var belowLeft = bricks[0][size.boardHeight - 2];
+
+            // hack to get the toContain to work -- we get a circular reference otherwise
+            leftNeighbour.adjacentBricks = new Array<Brick>();
+            belowRight.adjacentBricks = new Array<Brick>();
+            belowNeighbour.adjacentBricks = new Array<Brick>();
+            belowLeft.adjacentBricks = new Array<Brick>();
+            rightBrick.adjacentBricks = new Array<Brick>();
+
+            expect(topEdge.adjacentBricks.length).toEqual(5);
+            expect(topEdge.adjacentBricks).toContain(leftNeighbour);
+            expect(topEdge.adjacentBricks).toContain(belowRight);
+            expect(topEdge.adjacentBricks).toContain(belowNeighbour);
+            expect(topEdge.adjacentBricks).toContain(rightBrick);
+            expect(topEdge.adjacentBricks).toContain(belowLeft);
+        });
+
+
     });
 
     describe("The 'getNeighbourCount' method", () => {
