5fc8d4b67c23af20d32737e5606289d1d6df4115
par.dahlman <par.dahlman@valtech.se>
2014-01-02 19:22:06 +0100

    CLEAN: Removed GameState.js

--- MineSweaper/Script/Core/GameState.js
+++
@@ -1,7 +0,0 @@
-var GameState;
-(function (GameState) {
-    GameState[GameState["NotStarted"] = 0] = "NotStarted";
-    GameState[GameState["Ready"] = 1] = "Ready";
-    GameState[GameState["Ongoing"] = 2] = "Ongoing";
-    GameState[GameState["Finnished"] = 3] = "Finnished";
-})(GameState || (GameState = {}));
