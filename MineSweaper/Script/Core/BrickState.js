6908163de190715260f72696a9be7b976f2755ce
par.dahlman <par.dahlman@valtech.se>
2014-01-02 10:53:27 +0100

    Removed generated brick state js

--- MineSweaper/Script/Core/BrickState.js
+++
@@ -1,6 +0,0 @@
-var BrickState;
-(function (BrickState) {
-    BrickState[BrickState["Unknown"] = 0] = "Unknown";
-    BrickState[BrickState["FacingDown"] = 1] = "FacingDown";
-    BrickState[BrickState["FacingUp"] = 2] = "FacingUp";
-})(BrickState || (BrickState = {}));
