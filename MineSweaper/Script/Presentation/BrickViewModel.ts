040c722d508f15a3ff2f4bba4e6149d8211f550f
par.dahlman <par.dahlman@valtech.se>
2014-01-03 08:10:12 +0100

    Adding GameVm and adjusting BrickVm to it
    I had to make the Brick to a public property, so that the game can access it

--- MineSweaper/Script/Presentation/BrickViewModel.ts
+++ MineSweaper/Script/Presentation/BrickViewModel.ts
@@ -1,7 +1,7 @@
 ///<reference path="../../Librarys/typings/knockout/knockout.d.ts"/>
 
 class BrickViewModel {
-    private _brick: Brick;
+    public  Brick: Brick;
 
     public State: KnockoutObservable<BrickState> = ko.observable(BrickState.Unknown);
     public DisplayText: KnockoutComputed<string> = ko.computed(() => {
@@ -13,17 +13,17 @@ class BrickViewModel {
         }
 
         if (this.State() == BrickState.FacingUp) {
-            if (this._brick.type == BrickType.Bomb) {
+            if (this.Brick.type == BrickType.Bomb) {
                 return "boom";
             }
 
-            return this._brick.numberOfNormalNeighbours.toString();
+            return this.Brick.numberOfNormalNeighbours.toString();
             }
             return "";
         });
 
     constructor(brick: Brick) {
-        this._brick = brick;
+        this.Brick = brick;
         this.State(brick.state);
     }
 } 
---
+++ MineSweaper/Script/Presentation/GameViewModel.ts
@@ -0,0 +1,56 @@
+class GameViewModel {
+    private _game: Game = new Game();
+
+    public flip: (brickVm: BrickViewModel)=> void;
+    public Setup: ()=> void;
+
+    public BricksVms = ko.observableArray(new Array<Array<BrickViewModel>>());
+    public State: KnockoutObservable<GameState>;
+
+    public NumberOfRows: KnockoutObservable<number> = ko.observable(Game._defaultSettings.size.boardWidth);
+    public NumberOfColumn: KnockoutObservable<number> = ko.observable(Game._defaultSettings.size.boardHeight);
+    public Difficulty: KnockoutObservable<GameLevel> = ko.observable(Game._defaultSettings.level);
+
+    constructor() {
+        this.Setup = ()=> {
+            var settings = {
+                size: {
+                    boardWidth: this.NumberOfRows(),
+                    boardHeight: this.NumberOfColumn(),
+                },
+                level: this.Difficulty()
+            };
+
+            this._game.setup(settings);
+
+            this.BricksVms([]);
+            //this._game.bricks.forEach(row=> row.forEach(brick=> this.BricksVms.push(new BrickViewModel(brick))));
+
+            _.each(this._game.bricks, row=> {
+                var currentRow = new Array<BrickViewModel>();
+                
+                _.each(row, brick=> {
+                    currentRow.push(new BrickViewModel(brick));
+                });
+                this.BricksVms.push(currentRow);
+            });
+            this.State = ko.observable(this._game.state);
+
+
+        };
+
+        this.flip = (currentBrickVm: BrickViewModel)=> {
+            this._game.flip(currentBrickVm.Brick);
+
+            if (this.State() != this._game.state) {
+                this.State(this._game.state);
+            }
+
+            this.BricksVms().forEach(row => row.forEach(brickVm=> {
+                if (brickVm.Brick.state != brickVm.State()) {
+                    brickVm.State(brickVm.Brick.state);
+                }
+            }));
+        };
+    }
+}
